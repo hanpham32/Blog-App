f1523e15c93b671ca97c709e30fbc2fd5a80cbca
hanpham32 <hanpham3230@gmail.com>
2021-08-16 10:31:18 -0700

    Updates

--- blog/settings.py
+++ blog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5vu8)8+wqza3w0+n0bsna52c&759%p1#b88l*=+6xv6-j(o=x*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['hanwebblog.herokuapp.com', '127.0.0.1']
 
@@ -125,8 +125,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "static"),
+   os.path.join(BASE_DIR, 'static'),
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
