b2ad33567f8bd69e8640d63fcf1f96f83c891355
hanpham32 <hanpham3230@gmail.com>
2021-08-17 18:14:26 -0700

    Up and running

--- blog/settings.py
+++ blog/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5vu8)8+wqza3w0+n0bsna52c&759%p1#b88l*=+6xv6-j(o=x*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['hanwebblog.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['hanwebblog.herokuapp.com', '127.0.0.1', ',localhost']
 
 
 # Application definition
