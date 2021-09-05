a6007a99a424a1914cbf7b02842e6191a2c8b96d
hanpham32 <hanpham3230@gmail.com>
2021-09-04 22:59:09 -0700

    Added a rich text editor for post editing

--- myblog/models.py
+++ myblog/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from ckeditor.fields import RichTextField
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
     title_tag = models.CharField(max_length=255, default='')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     preview = models.CharField(max_length=255, default='')
-    body = models.TextField()
-
+    # body = models.TextField()
+    body = RichTextField(blank=True, null=True)
     date_created = models.DateTimeField(auto_now_add=True, blank=True)
     last_modified = models.DateTimeField(auto_now=True, blank=True)
 
