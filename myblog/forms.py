879be4dc131855fbb93cab5d9e43276e72f80fbc
Han Pham <hanpham@Hans-MacBook-Pro.local>
2021-10-01 23:52:15 -0700

    Commited

--- myblog/forms.py
+++ myblog/forms.py
@@ -5,10 +5,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'title_tag', 'author','preview', 'body')
-
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Title'}),
-            'title_tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Tag(s)'}),
+            'title_tag': forms.Select(attrs={'class': 'form-control', 'placeholder':'Tag(s)'}),
             'author': forms.Select(attrs={'class': 'form-control', 'placeholder':'Author'}),
             'preview': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Preview of post'}),
             'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder':'Content'}),
--- myblog/models.py
+++ myblog/models.py
@@ -3,16 +3,21 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from ckeditor.fields import RichTextField
 
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    title_tag = models.CharField(max_length=255, default='')
+    title_tag = models.ForeignKey(Tag, blank=True, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     preview = models.CharField(max_length=255, default='')
-    # body = models.TextField()
     body = RichTextField(blank=True, null=True)
     date_created = models.DateTimeField(auto_now_add=True, blank=True)
     last_modified = models.DateTimeField(auto_now=True, blank=True)
-
+    
     def __str__(self):
         return self.title
 
